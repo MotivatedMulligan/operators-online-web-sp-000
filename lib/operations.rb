48c91a6ab9d388f570566a23c7e55a30a773523e
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-01-31 20:56:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
+require 'pry'
+
 def unsafe?(speed)
-if      "#{speed}" < 40 : 60 < "#{speed}"
+if      speed < 40 : 60 < speed
  return true
 else
 	return false
 end
 
-
+binding.pry
 
 def not_safe?(speed)
-	speed ? :
+	speed  < 40 ||  ? :
 end
